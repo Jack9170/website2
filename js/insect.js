7cd299c8c7ea0031d02b05a5f4b7d99eb8a0b4a8
Jack9170 <142847417+Jack9170@users.noreply.github.com>
2024-04-11 20:50:36 +0000

    Thu, Apr 11, 2024, 1:50 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -4,13 +4,13 @@ const game_container = document.getElementById('game-container')
 const start_btn = document.getElementById('start-btn')
 const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
-const message = document.getElmentById('message')
+const message = document.getElementById('message')
 let seconds = 0
 let score = 0
 let selected_insect = {}
 
 start_btn.addEventListener('click', () => {
-    screens.classList.add('up')
+    screens[0].classList.add('up')
 })
 
 choose_insect_btns.forEach(btn => {
@@ -19,5 +19,6 @@ choose_insect_btns.forEach(btn => {
         const alt = img.getAttribute('alt')
         const src = img.getAttribute('src')
         screens [1].classList.add('up')
+        selected_insect = {src, alt}
     })
 })
